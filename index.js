0bf9c66ca5f0e0af79f21ca80528be84a03ae7b3
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-24 10:48:25 -0700

    Init

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './background';
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 38983;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
