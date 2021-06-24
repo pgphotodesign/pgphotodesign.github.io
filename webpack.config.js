34be8c645e45819a052a126ea448932a92ee0707
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-24 16:45:13 -0700

    Restructure

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './client/src/index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'client', 'dist'),
     filename: 'bundle.js',
   },
   module: {
